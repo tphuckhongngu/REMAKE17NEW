191a30ca189e7eba4cfccc6288092441f86f6360
hongthilai3@gmail.com <hongthilai3@gmail.com>
2025-12-15 19:01:03 +0700

    thay doiamthanh

--- sounds.py
+++ sounds.py
@@ -1,35 +1,28 @@
 # sounds.py
 import pygame
-import os
 
 class SoundManager:
-    @staticmethod
     def init_mixer():
-        if not pygame.mixer.get_init():
-            pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=512)
+        pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=512)
 
-    @staticmethod
-    def play_background_music(volume=0.4):
-        base_path = os.path.dirname(__file__)
-        sound_path = os.path.join(base_path, 'background_music.mp3')
-
-        pygame.mixer.music.load(sound_path)
+    def play_background_music(volume=-100000):
+        pygame.mixer.music.load('background_music.mp3')
         pygame.mixer.music.set_volume(volume)
         pygame.mixer.music.play(-1)
 
-    @staticmethod
-    def play_menu_music(volume=0.4):
-        base_path = os.path.dirname(__file__)
-        sound_path = os.path.join(base_path, 'menu_music.mp3')
-
-        pygame.mixer.music.load(sound_path)
+    def play_menu_music(volume=-10000):
+        pygame.mixer.music.load('menu_music.mp3')
         pygame.mixer.music.set_volume(volume)
         pygame.mixer.music.play(-1)
 
-    @staticmethod
     def stop_music():
         pygame.mixer.music.stop()
 
-    @staticmethod
     def fadeout_background_music(ms=1000):
         pygame.mixer.music.fadeout(ms)
+    def set_volume(volume):
+        pygame.mixer.music.set_volume(volume)
+    def play_click_sound():
+        click_sound = pygame.mixer.Sound('click.mp3')  # đường dẫn file của bạn
+        click_sound.set_volume(0.6)  # âm lượng vừa phải, bạn có thể chỉnh 0.3 - 0.8
+        click_sound.play()
