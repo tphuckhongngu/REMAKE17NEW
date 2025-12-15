b3b2ee9f60f65b804512e293d940ed469f9b844f
Tran Chi Huong <huongprovn001@gmail.com>
2025-12-15 15:43:18 +0700

    sửa file sounds.py chạy được trên mọi folder

--- sounds.py
+++ sounds.py
@@ -1,22 +1,35 @@
 # sounds.py
 import pygame
+import os
 
 class SoundManager:
+    @staticmethod
     def init_mixer():
-        pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=512)
+        if not pygame.mixer.get_init():
+            pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=512)
 
-    def play_background_music(volume=-100000):
-        pygame.mixer.music.load('background_music.mp3')
+    @staticmethod
+    def play_background_music(volume=0.4):
+        base_path = os.path.dirname(__file__)
+        sound_path = os.path.join(base_path, 'background_music.mp3')
+
+        pygame.mixer.music.load(sound_path)
         pygame.mixer.music.set_volume(volume)
         pygame.mixer.music.play(-1)
 
-    def play_menu_music(volume=-10000):
-        pygame.mixer.music.load('menu_music.mp3')
+    @staticmethod
+    def play_menu_music(volume=0.4):
+        base_path = os.path.dirname(__file__)
+        sound_path = os.path.join(base_path, 'menu_music.mp3')
+
+        pygame.mixer.music.load(sound_path)
         pygame.mixer.music.set_volume(volume)
         pygame.mixer.music.play(-1)
 
+    @staticmethod
     def stop_music():
         pygame.mixer.music.stop()
 
+    @staticmethod
     def fadeout_background_music(ms=1000):
         pygame.mixer.music.fadeout(ms)
