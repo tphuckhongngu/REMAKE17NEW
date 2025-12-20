30802f4e563264fdeee02a7dc101363fd8a523b1
Tran Chi Huong <huongprovn001@gmail.com>
2025-12-20 21:23:48 +0700

    lỗi nhiều quá, quay về cũ

--- sounds.py
+++ sounds.py
@@ -1,28 +1,55 @@
 # sounds.py
 import pygame
+import os
+
+# ===== BASE DIR =====
+BASE_DIR = os.path.dirname(__file__)
+
+def sound_path(filename):
+    return os.path.join(BASE_DIR, filename)
+
 
 class SoundManager:
+    @staticmethod
     def init_mixer():
-        pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=512)
+        if not pygame.mixer.get_init():
+            pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=512)
 
-    def play_background_music(volume=-100000):
-        pygame.mixer.music.load('background_music.mp3')
-        pygame.mixer.music.set_volume(volume)
-        pygame.mixer.music.play(-1)
+    @staticmethod
+    def play_background_music(volume=0.5):
+        try:
+            pygame.mixer.music.load(sound_path('background_music.mp3'))
+            pygame.mixer.music.set_volume(volume)
+            pygame.mixer.music.play(-1)
+        except Exception as e:
+            print("Loi load background_music:", e)
 
-    def play_menu_music(volume=-10000):
-        pygame.mixer.music.load('menu_music.mp3')
-        pygame.mixer.music.set_volume(volume)
-        pygame.mixer.music.play(-1)
+    @staticmethod
+    def play_menu_music(volume=0.4):
+        try:
+            pygame.mixer.music.load(sound_path('menu_music.mp3'))
+            pygame.mixer.music.set_volume(volume)
+            pygame.mixer.music.play(-1)
+        except Exception as e:
+            print("Loi load menu_music:", e)
 
+    @staticmethod
     def stop_music():
         pygame.mixer.music.stop()
 
+    @staticmethod
     def fadeout_background_music(ms=1000):
         pygame.mixer.music.fadeout(ms)
+
+    @staticmethod
     def set_volume(volume):
         pygame.mixer.music.set_volume(volume)
+
+    @staticmethod
     def play_click_sound():
-        click_sound = pygame.mixer.Sound('click.mp3')  # đường dẫn file của bạn
-        click_sound.set_volume(0.6)  # âm lượng vừa phải, bạn có thể chỉnh 0.3 - 0.8
-        click_sound.play()
+        try:
+            click_sound = pygame.mixer.Sound(sound_path('click.mp3'))
+            click_sound.set_volume(0.6)
+            click_sound.play()
+        except Exception as e:
+            print("Loi load click sound:", e)
